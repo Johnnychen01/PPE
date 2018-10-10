2a7c471825db4d88cc2d02e09f2baaf7e2bb28bc
Johnnychen01 <43983653+Johnnychen01@users.noreply.github.com>
2018-10-10 11:19:09 +0200

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -1,65 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define NLIGNES 1000
 
-int main (void)
+int Ajout()
 {
 	FILE *fp;
-	char choix;
-	printf("\tSaisir a pour ajouter une personne dans le repertoire");
-	printf("\n\tSaisir b pour afficher le contenue du repertoire");
-	printf("\n\tSaisir c pour rechercher une personne dans le repertoire");
-	printf("\n\tSaisir d pour supprimer une personne du repertoire");
-	printf("\n\tSaisir e pour supprimer le repertoire\n");
-	scanf("%c",&choix);
-	getchar();
-	
-	if (choix == 'a')
-	{
-		int p;
-		char ID[20], nom[20], prenom[20], mail[35];
-		fp= fopen("repertoire.txt","a+");
+	int telephone;
+	char surname[20], prenom[20], mail[35];
+	fp= fopen("repertoire.txt","a+");
+	printf("						~~~~~~~~~~~~~~~~~~~~~~Ajout d\'un utilisateur~~~~~~~~~~~~~~~~~~~~~~\n");
+	srand(time(NULL));
+	int ID = rand()%100 + 1;
+	printf("									Votre ID est :");
+	printf(" %d\n",ID);
+	printf("\n									Nom : ");
+	scanf("	%s", &surname);
+	printf("\n									Prenom : ");
+	scanf("	%s", &prenom);
+	printf("\n									Telephone : ");
+	scanf("	%d", &telephone);
+	printf("\n									Mail : ");
+	scanf("%s", &mail);
+	printf("						~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		
-		printf("Identifiant :");
-		scanf("%s", &ID);
-		printf("\nNom : ");
-		scanf("%s", &surname);
-		printf("\nPrenom : ");
-		scanf("%s", &prenom);
-		printf("\nTelephone : ");
-		scanf("%d", &p);
-		printf("\nMail : ");
-		scanf("%s", &mail);
-		
-		fprintf(fp,"\n%s\t\t\t\t%s\t\t\t%s\t\t0%d\t\t %s", ID, surname, prenom, p, mail);		
-	}
-	
-	if (choix== 'b')
-	{
-		fp= fopen("repertoire.txt","r");
+	fprintf(fp,"\n%d\t\t\t\t%s\t\t\t%s\t\t\t0%d\t\t %s", ID, surname, prenom, telephone, mail);	
+}
+
+int Afficher()
+{
+	FILE *fp;
+	fp= fopen("repertoire.txt","r");
 		int c = fgetc(fp);
 		while (c != EOF)
 		{
 			printf("%c",c);
 			c = fgetc(fp);
 		}
-	}
+}
 
-	if (choix == 'c')
-	{
+int Rechercher()
+{
 		
+}
+
+int Supprimer()
+{
+	
+}
+
+int Tout_supprimer()
+{
+	FILE *fp;
+	fp=fopen("repertoire.txt","w");
+	fclose(fp); 
+	printf("Le contenu du repertoire a ete supprime");
+	fp=fopen("repertoire.txt","a+");
+	fprintf(fp,"Identifiant			Nom			Prenom			Telephone		Mail");
+}
+
+int main()
+{
+	FILE *fp;
+	char choix;
+	printf("							~~~~~~~~~~~~~~~~~~~~~~ MENU ~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("							(1) 	Ajouter une personne dans le repertoire");
+	printf("\n							(2) 	Afficher le contenue du repertoire");
+	printf("\n							(3)	Rechercher une personne dans le repertoire");
+	printf("\n							(4)	Supprimer une personne du repertoire");
+	printf("\n							(5)	Supprimer le repertoire\n");
+	printf("							~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	scanf("%c",&choix);
+	getchar();
+	
+	if (choix == '1')
+	{
+		Ajout();	
 	}
 	
-	if (choix == 'd')
+	if (choix== '2')
 	{
+		Afficher();
 	}
 	
-	if (choix =='e')
+	
+	if (choix == '3')
 	{
-		fp=fopen("repertoire.txt","w");
-		fclose(fp); 
-		fp=fopen("repertoire.txt","a+");
-		fprintf(fp,"1-Identifiant			2-Nom			3-Prenom			4-Telephone		5-Mail");
+		Rechercher();
+	}	
+	
+	if (choix == '4')
+	{
+		Supprimer();
+	}
+	
+	if (choix =='5')
+	{
+		Tout_supprimer();
 	}
 }
+
